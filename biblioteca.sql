fe81be1a4b1b1aee1a90ab7d7cc11ead47a8656c
dgordillo-arch <dgordillo@sagradoscorazonesmsq.edu.co>
2026-05-15 12:22:42 -0500

    Update biblioteca.sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,37 +1,56 @@
-CREATE TABLE "mytable" (
-  "CREATE DATABASE BibliotecaDB;" text
+-- =========================================
+-- A. BASE DE DATOS BIBLIOTECA
+-- =========================================
+
+CREATE DATABASE BibliotecaDB;
+USE BibliotecaDB;
+
+-- =====================
+-- TABLA AUTORES
+-- =====================
+
+CREATE TABLE Autores(
+    AutorID INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    Nacionalidad VARCHAR(100)
 );
 
-INSERT INTO "mytable" ("CREATE DATABASE BibliotecaDB;")
-VALUES
-('USE BibliotecaDB;'),
-('CREATE TABLE Autores('),
-('AutorID INT PRIMARY KEY'),
-('Nombre VARCHAR(100)'),
-('Apellido VARCHAR(100)'),
-('Nacionalidad VARCHAR(100)'),
-(');'),
-('CREATE TABLE Libros('),
-('LibroID INT PRIMARY KEY'),
-('Titulo VARCHAR(100)'),
-('AutorID INT'),
-('Genero VARCHAR(100)'),
-('ISBN VARCHAR(50)'),
-('FOREIGN KEY (AutorID) REFERENCES Autores(AutorID)'),
-(');'),
-('CREATE TABLE Miembros('),
-('MiembroID INT PRIMARY KEY'),
-('Nombre VARCHAR(100)'),
-('Apellido VARCHAR(100)'),
-('Direccion VARCHAR(150)'),
-('Telefono VARCHAR(20)'),
-(');'),
-('CREATE TABLE Prestamos('),
-('PrestamoID INT PRIMARY KEY'),
-('LibroID INT'),
-('MiembroID INT'),
-('Fecha_Prestamo DATE'),
-('Fecha_Devolucion DATE'),
-('FOREIGN KEY (LibroID) REFERENCES Libros(LibroID)'),
-('FOREIGN KEY (MiembroID) REFERENCES Miembros(MiembroID)'),
-(');');
+-- =====================
+-- TABLA LIBROS
+-- =====================
+
+CREATE TABLE Libros(
+    LibroID INT PRIMARY KEY,
+    Titulo VARCHAR(150),
+    AutorID INT,
+    Genero VARCHAR(100),
+    ISBN VARCHAR(50),
+    FOREIGN KEY (AutorID) REFERENCES Autores(AutorID)
+);
+
+-- =====================
+-- TABLA MIEMBROS
+-- =====================
+
+CREATE TABLE Miembros(
+    MiembroID INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    Direccion VARCHAR(150),
+    Telefono VARCHAR(20)
+);
+
+-- =====================
+-- TABLA PRESTAMOS
+-- =====================
+
+CREATE TABLE Prestamos(
+    PrestamoID INT PRIMARY KEY,
+    LibroID INT,
+    MiembroID INT,
+    Fecha_Prestamo DATE,
+    Fecha_Devolucion DATE,
+    FOREIGN KEY (LibroID) REFERENCES Libros(LibroID),
+    FOREIGN KEY (MiembroID) REFERENCES Miembros(MiembroID)
+);
